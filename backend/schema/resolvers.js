1d52b37c9f47eac39f17a7fe88e8dbea01be33b3
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2022-05-17 05:58:45 -0700

    jwt

--- backend/schema/resolvers.js
+++ backend/schema/resolvers.js
@@ -1,5 +1,9 @@
-const { UserInputError } = require('apollo-server');
+const {UserInputError} = require('apollo-server');
 const User = require('../Models/UserModel');
+const jwt = require('jsonwebtoken');
+const {secret} = require('../Utils/config')
+const {auth} = require("../Utils/passport");
+auth();
 
 module.exports = {
     Query: {
@@ -7,10 +11,15 @@ module.exports = {
             const allUsers = await User.find({});
             return allUsers;
         },
+        getProfile: async (_, args) => {
+            const {userId} = args;
+            const getUser = await User.findById(userId);
+            return getUser;
+        },
     },
     Mutation: {
         register: async (_, args) => {
-            const { username, password } = args;
+            const {username, password} = args;
             const user = new User({
                 username,
                 password
@@ -18,28 +27,68 @@ module.exports = {
 
             const savedUser = await user.save();
 
+            const payload = {id: user._id, username: user.username};
+            const token = jwt.sign(payload, secret, {
+                expiresIn: 1008000
+            });
+
             return {
                 id: savedUser._id,
                 username: savedUser.username,
-                password: savedUser.password,
+                token
             };
         },
 
         login: async (_, args) => {
-            const { username, password } = args;
+            const {username, password} = args;
             const user = await User.findOne({
-                username: { $regex: new RegExp('^' + username + '$', 'i')},
-                password: { $regex: new RegExp('^' + password + '$', 'i')}
+                username: {$regex: new RegExp('^' + username + '$', 'i')},
+                password: {$regex: new RegExp('^' + password + '$', 'i')}
             });
 
             if (!user) {
-                throw new UserInputError("User or password is invalid");
+                throw new UserInputError("Username or password is invalid");
             }
+
+            const payload = {id: user._id, username: user.username};
+            const token = jwt.sign(payload, secret, {
+                expiresIn: 1008000
+            });
+
             return {
                 id: user._id,
                 username: user.username,
+                token
 
             };
+        },
+
+        editProfile: async (_, args) => {
+            const {userId, name, img, street, state, city, country, zipCode, email, phoneNum, birthDay} = args;
+
+
+            const updatedProfile= {
+                name, img, street, state, city, country, zipCode, email, phoneNum, birthDay
+            };
+
+            try {
+                const user = await User.findById(userId);
+                if (!user) {
+                    throw new UserInputError(
+                        `User with ID: ${userId} does not exist in DB.`
+                    );
+                }
+
+                const updatedUser = await User.findByIdAndUpdate(
+                    userId,
+                    updatedProfile,
+                    {new: true}
+                )
+
+                return updatedUser;
+            } catch (err) {
+                throw new UserInputError(err);
+            }
         },
     },
 };
