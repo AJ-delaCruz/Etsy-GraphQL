2f149bb7637a8cfd211c1a5cc62e8acfc39256a9
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2022-05-16 17:44:59 -0700

    added appolloprovider and client to connect to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {ApolloClient, InMemoryCache, ApolloProvider} from "@apollo/client";
+// components
+import Main from './components/Main';
+
+
+// apollo client setup
+const client = new ApolloClient({
+    uri: 'http://localhost:3001/graphql',
+    cache: new InMemoryCache(),
+});
+
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+
+                <ApolloProvider client={client}>
+                    <div id="main">
+                        <h1>hello</h1>
+                        <Main/>
+                    </div>
+                </ApolloProvider>
+            </BrowserRouter>
+
+        );
+    }
 }
 
 export default App;
