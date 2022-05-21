34050cc06040e628ccaea3fb01518941ce4b3acb
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2022-05-21 12:22:31 -0700

    updated login/signup css

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { LOGIN_USER_MUTATION } from "../../GraphQL/Mutations";
 import { useMutation } from "@apollo/client";
 import jwt_decode from 'jwt-decode';
+import styled from "styled-components";
+import {Button} from "@mui/material";
 
 function Login() {
     const [username, setUsername] = useState("");
@@ -40,24 +42,76 @@ function Login() {
 
     };
     return (
-        <div>
-            <input
-                type="text"
-                placeholder="Username"
-                onChange={(e) => {
-                    setUsername(e.target.value);
-                }}
-            />
-            <input
-                type="text"
-                placeholder="Password"
-                onChange={(e) => {
-                    setPassword(e.target.value);
-                }}
-            />
-            <button onClick={login}> Login</button>
-        </div>
+        <Container>
+            <Wrapper>
+                <Title>LOG IN</Title>
+                <Form>
+                    <Input placeholder="username" type = "text" onChange={(e) => {
+                        setUsername(e.target.value);
+                    }}/>
+
+                    <Input placeholder="password" type = "password" onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}/>
+
+                    <Button style={{
+                        width: "50%",
+                        border: "none",
+                        padding: "15px",
+                        backgroundColor: "blue",
+                        color: "white",
+                        cursor: "pointer",
+                        margin: "10px",
+                    }}
+                            onClick={login}>
+                        Log In
+                    </Button>
+                </Form>
+            </Wrapper>
+        </Container>
     );
 }
+
+const Container = styled.div`
+width: 100vw;
+height: 100vh;
+display: flex;
+align-items: center;
+justify-content: center;
+`;
+
+const Wrapper = styled.div`
+width: 40%;
+padding: 20px;
+background-color: #f5fbfd;;
+`;
+
+const Title = styled.h1`
+font-size: 24px;
+font-weight: 300;
+display: flex;
+align-items: center;
+justify-content: center;
+`;
+
+const Form = styled.form`
+display: flex;
+flex-wrap: wrap;
+flex-direction: column;
+align-items: center;
+`;
+
+const Input = styled.input`
+flex: 1;
+width: 500px;
+margin: 20px 10px 0px 0px;
+padding: 10px;
+text-align: center
+`;
+
+
+
+
+
 
 export default Login;
--- frontend/src/components/Signup/Signup.js
+++ frontend/src/components/Signup/Signup.js
@@ -1,46 +1,120 @@
-import React, { useState } from "react";
-import { CREATE_USER_MUTATION } from "../../GraphQL/Mutations";
-import { useMutation } from "@apollo/client";
-
+import React, {useState} from "react";
+import {CREATE_USER_MUTATION} from "../../GraphQL/Mutations";
+import {useMutation} from "@apollo/client";
+import styled from "styled-components";
+import {Navigate} from "react-router";
+import {useHistory, Redirect} from "react-router-dom";
+import {Button} from "@mui/material";
 function Signup() {
+
+    const [name, setName] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
     //post
-    const [register, { error }] = useMutation(CREATE_USER_MUTATION);
+    const [register, {error}] = useMutation(CREATE_USER_MUTATION);
 
     //button to register
     const addUser = () => {
         register({
             variables: {
+                name: name,
                 username: username,
                 password: password,
             },
-        });
 
+        });
+        // history.push("/");
+        // window.location.href ="/home";
         if (error) {
             console.log(error);
         }
     };
+
+    // let redirectVar = null;
+    // if (this.state.authFlag) {
+    //     redirectVar =  <Navigate to="/home"/>
+    //
+    // }
     return (
-        <div>
-            <input
-                type="text"
-                placeholder="Username"
-                onChange={(e) => {
-                    setUsername(e.target.value);
-                }}
-            />
-            <input
-                type="text"
-                placeholder="Password"
-                onChange={(e) => {
-                    setPassword(e.target.value);
-                }}
-            />
-            <button onClick={addUser}> Register</button>
-        </div>
+
+        <Container>
+            {/*{ redirectVar}*/}
+            <Wrapper>
+                <Title>CREATE AN ACCOUNT</Title>
+                <Form>
+
+                    <Input placeholder="Name" onChange={(e) => {
+                        setName(e.target.value);
+                    }}/>
+
+
+                    <Input placeholder="username" onChange={(e) => {
+                        setUsername(e.target.value);
+                    }}/>
+
+                    <Input placeholder="password" type = "password" onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}/>
+
+                    <Button style={{
+                        width: "50%",
+                        border: "none",
+                        padding: "15px",
+                        backgroundColor: "blue",
+                        color: "white",
+                        cursor: "pointer",
+                        margin: "10px",
+                    }}
+                            onClick={addUser}>
+                        Register
+                    </Button>
+                </Form>
+            </Wrapper>
+        </Container>
     );
 }
 
+const Container = styled.div`
+width: 100vw;
+height: 100vh;
+display: flex;
+align-items: center;
+justify-content: center;
+`;
+
+const Wrapper = styled.div`
+width: 40%;
+padding: 20px;
+background-color: #f5fbfd;;
+`;
+
+const Title = styled.h1`
+font-size: 24px;
+font-weight: 300;
+display: flex;
+align-items: center;
+justify-content: center;
+`;
+
+const Form = styled.form`
+display: flex;
+flex-wrap: wrap;
+flex-direction: column;
+align-items: center;
+`;
+
+const Input = styled.input`
+flex: 1;
+width: 500px;
+margin: 20px 10px 0px 0px;
+padding: 10px;
+text-align: center
+`;
+
+
+
+
+
 export default Signup;
+
